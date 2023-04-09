4f1d9c6c4970474b63aed89844ee5b5a4b1cca1c
Wouter Vanden Hove <wouter@libranet.eu>
2023-04-09 15:35:43 +0200

    extend

--- scripts/install-cookiecutter.sh
+++ scripts/install-cookiecutter.sh
@@ -3,9 +3,17 @@
 # set -x  # enable for verbose output
 set -e # failing commands cause the shell script to exit immediately
 
-#      curl -sSL https://install.python-poetry.org | python3 -
 
 NOW=$(date +"%Y-%m-%d %H:%M:S")
+
+# INSTALL_DIR=$(realpath -s "$1")
+INSTALL_DIR=${1:-/opt/tools/cookiecutter}
+BIN_DIR=${INSTALL_DIR}/bin
+
+# get parameters from the input
+# ENV_NAME=${1:.venv}
+PYTHON_VERSION=${2:-3.11}   # set default python-version to 3.11
+
 
 function help_() { # don't shadow help-builtin
     echo "Steps:"
@@ -16,14 +24,22 @@ function help_() { # don't shadow help-builtin
     exit
 }
 
+function create_venv() {
+    echo -e "Create venv in ${INSTALL_DIR}"
+    mkdir -p ${INSTALL_DIR}
+    # PYENV_VERSION=${PYTHON_VERSION} python -m venv "$ENV_NAME"
+    PYENV_VERSION=${PYTHON_VERSION} python -m venv "$INSTALL_DIR"
+
+}
+
 function update_pip() {
     echo -e "Updating pip."
-    pip install -U pip
+    ${BIN_DIR}/pip install -U pip
 }
 
 function install_cookiecutter() {
     echo -e "Installing cookiecutter."
-    pip install cookiecutter
+     ${BIN_DIR}/pip install -U cookiecutter
 }
 
 function symlink_poetry() {
@@ -31,7 +47,15 @@ function symlink_poetry() {
     cd ~/bin && ln -s ~/.local/share/pypoetry/venv/bin/poetry poetry && cd -
 }
 
+function create_dir_structure() {
+    echo -e "Installing ${INSTALL_DIR}/var."
+    mkdir -p ${INSTALL_DIR}/etc
+    mkdir -p ${INSTALL_DIR}/var
+}
+
 # main program
+create_venv
 update_pip
 install_cookiecutter
-symlink_cookiecutter
+# symlink_cookiecutter
+create_dir_structure
